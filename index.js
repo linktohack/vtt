00545a6c2866b0844300b09d62febe78b33bff78
Quang-Linh LE <linktohack@gmail.com>
2017-09-08 10:41:38 +0200

    Refactor handlers

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const fetch = require('node-fetch');
-const srt2vtt = require('srt-to-vtt')
-const ass2vtt = require('ass-to-vtt')
+const srt2vtt = require('srt-to-vtt');
+const ass2vtt = require('ass-to-vtt');
 
 const abab = require('abab');
 const https = require('https');
@@ -11,7 +11,23 @@ const agent = new https.Agent({
     rejectUnauthorized: false
 });
 
-app.use(cors())
+const handlerFor = (transformer) => (req, res) => {
+    res.setHeader('Content-Type', 'text/vtt');
+    const url = abab.atob(req.query.hash);
+    if (typeof url !== 'string') {
+        res.end('WEBTT FILE\n\n1\n0 --> 10\nError: invalid url}');
+        return;
+    }
+    fetch(url, {agent})
+        .then(res2 => {
+            res2.body.pipe(transformer()).pipe(res);
+        })
+        .catch(e => {
+            res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: ${e && e.message || 'unknown'}`);
+        });
+};
+
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.end(['Convert and set the correct vtt mime type for srt and ass (and vtt)',
@@ -19,58 +35,15 @@ app.get('/', function (req, res) {
              '\t/ass?hash=:hash',
              '\t/vtt?hash=:hash',
              'where `:hash` is `btoa(url)`',
-             'https://github.com/linktohack/vtt'].join('\n'))
-})
+             'https://github.com/linktohack/vtt'].join('\n'));
+});
 
-app.get('/srt', function (req, res) {
-    res.setHeader('Content-Type', 'text/vtt')
-    const url = abab.atob(req.query.hash)
-    if (typeof url !== 'string') {
-        res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: invalid url}`)
-        return
-    }
-    console.log('url', url)
-    fetch(url, { agent })
-        .then(res2 => {
-            res2.body.pipe(srt2vtt()).pipe(res)
-        })
-        .catch(e => {
-            res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: ${e && e.message || 'unknown'}`)
-        })
-})
+app.get('/srt', handlerFor(srt2vtt));
 
-app.get('/ass', function (req, res) {
-    res.setHeader('Content-Type', 'text/vtt')
-    const url = abab.atob(req.query.hash)
-    if (typeof url !== 'string') {
-        res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: invalid url}`)
-        return
-    }
-    fetch(url, { agent })
-        .then(res2 => {
-            res2.body.pipe(ass2vtt()).pipe(res)
-        })
-        .catch(e => {
-            res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: ${e && e.message || 'unknown'}`)
-        })
-})
+app.get('/ass', handlerFor(ass2vtt));
 
-app.get('/vtt', function (req, res) {
-    res.setHeader('Content-Type', 'text/vtt')
-    const url = abab.atob(req.query.hash)
-    if (typeof url !== 'string') {
-        res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: invalid url}`)
-        return
-    }
-    fetch(url, { agent })
-        .then(res2 => {
-            res2.body.pipe.pipe(res)
-        })
-        .catch(e => {
-            res.end(`WEBTT FILE\n\n1\n0 --> 10\nError: ${e && e.message || 'unknown'}`)
-        })
-})
+app.get('/vtt', handlerFor(it => it));
 
 app.listen(process.env.PORT || 8000, function () {
-    console.log(`Server listening on port ${process.env.PORT || 8000}!`)
-})
+    console.log(`Server listening on port ${process.env.PORT || 8000}!`);
+});
